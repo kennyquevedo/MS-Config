2d1170e11a7d0cb3cf716cc33ff05699d3fd0ead
Javier Saldaña <cjaviersaldana@outlook.com>
2019-07-06 19:00:54 -0500

    added new programs and organice

--- MS-Setup.ps1
+++ MS-Setup.ps1
@@ -1,5 +1,15 @@
+#tools
 choco install 7zip.install -y
-choco install vscode -y 
-choco install sql-server-management-studio -y 
 choco install conemu -y 
 choco install sharex -y 
+choco install fiddler -y
+choco install postman -y
+
+#code editor
+choco install vscode -y 
+choco install sql-server-management-studio -y 
+choco install azure-data-studio -y
+choco install visualstudio2019enterprise -y
+
+#environment
+choco install docker-desktop -y

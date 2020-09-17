0b65e3ebfcb7c2b1f53190371b172107af66337f
Kenny Quevedo <56044177+kennyquevedo@users.noreply.github.com>
2020-09-17 15:58:52 -0500

    Add files via upload

---
+++ MSConfig_dev.ps1
@@ -0,0 +1,8 @@
+#tools
+choco install visualstudio2019community -y
+choco install vscode -y 
+choco install sql-server-management-studio -y 
+choco install azure-data-studio -y
+
+choco install fiddler -y
+choco install postman -y

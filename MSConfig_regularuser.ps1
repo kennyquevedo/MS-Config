cb4c469349a548c76169109f322d273940a403ba
Kenny Quevedo <56044177+kennyquevedo@users.noreply.github.com>
2020-09-17 15:58:08 -0500

    Add files via upload

---
+++ MSConfig_regularuser.ps1
@@ -0,0 +1,21 @@
+#tools
+choco install 7zip.install -y
+choco install sharex -y 
+choco install gimp -y
+choco install k-litecodecpackfull -y
+choco install teamviewer -y
+choco install ditto.install -y
+
+#Browser
+choco install googlechrome -y
+choco install firefox -y
+choco install opera -y
+choco install opera-developer -y
+
+#others
+choco install steam -y
+choco install folder_size -y
+choco install origin -y
+choco install uplay -y
+choco install epicgameslauncher -y
+choco install goggalaxy -y

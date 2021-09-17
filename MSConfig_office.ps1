76e79768789798f132de1a75f699665a45e8adad
Kenny Quevedo <kenny.quevedo@morningstar.com>
2021-09-17 10:32:58 -0500

    Add office utilites

---
+++ MSConfig_office.ps1
@@ -0,0 +1,18 @@
+#tools
+choco install 7zip.install -y
+choco install sharex -y 
+choco install gimp -y
+choco install k-litecodecpackfull -y
+choco install ditto.install -y
+choco install zoom -y
+
+#Browser
+choco install googlechrome -y
+choco install firefox -y
+choco install opera -y
+
+#others
+choco install folder_size -y
+choco install adobereader -y
+choco install slack -y
+choco install screenpresso -y

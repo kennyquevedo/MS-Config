7012cd0eb8638e492ef93c805411c14f1dc074cd
Kenny Quevedo <56044177+kennyquevedo@users.noreply.github.com>
2022-01-23 14:03:52 -0500

    Update MSConfig_dev.ps1

--- MSConfig_dev.ps1
+++ MSConfig_dev.ps1
@@ -1,4 +1,5 @@
 #tools
+choco install visualstudio2022community -y -install-arguments --installpath "D:\devtools\visualstudio"
 choco install vscode -y 
 choco install sql-server-management-studio -y 
 choco install azure-data-studio -y
